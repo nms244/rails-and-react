f53215da70e7283ea436bc40efe8478952d04fd4
nomos244 <nomos.geek@gmail.com>
2023-01-25 16:24:49 +0900

    TOPに表示されるのをtasks#showに変更

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     const getTrendMovie = async () => {
-      const response = await axios.get(requests.tasks);
+      const response = await axios.get(requests.tasks+'1');
       console.log(response.data);
       setTest(response.data);
     }
