b385daf605409e04c287d75f185dae8c541fe357
nomos244 <nomos.geek@gmail.com>
2023-01-26 01:26:47 +0900

    Task一覧ページを整える

--- frontend/src/components/pages/TaskIndex.jsx
+++ frontend/src/components/pages/TaskIndex.jsx
@@ -4,6 +4,7 @@ import { TaskShow } from '../organisms/TaskShow'
 export const TaskIndex = (props) => {
   const { tasks } = props
 
+
   return (
     <main className="flex mb-16">
       <ul className="flex flex-col min-w-[320px] max-w-xl w-full mr-auto">
