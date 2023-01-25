8b4bf7bcd044044ab7c74e069917350c48c4f4cd
nomos244 <nomos.geek@gmail.com>
2023-01-25 23:26:52 +0900

    コントローラのcreateアクションをService Objectを用いたロジックに変更

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,19 +3,22 @@ class Api::V1::TasksController < ApplicationController
   def create
     # CreateTask.call(create_task_params)
     ActiveRecord::Base.transaction do
-      @task = Task.create!(create_task_params)
-      loop_start = Arrangement.days[:mon]
-      loop_end = Arrangement.days[:fri]
-      loop_start.upto(loop_end) do |day|
-        @task.arrangements.create!(
-          day: day, goal_per_day: 0, done_per_day: 0
-        )
-      end
-      @task.rearrange
-      render json: { controller_action: 'tasks#create', text: 'taskとarrangementの保存に成功しました' }
+      task = CreateTask.call(create_task_params)
+      render json: { controller_action: 'tasks#create', task: task, arrangements: task.arrangements }
+
+      # task = Task.create!(create_task_params)
+      # loop_start = Arrangement.days[:mon]
+      # loop_end = Arrangement.days[:fri]
+      # loop_start.upto(loop_end) do |day|
+      #   task.arrangements.create!(
+      #     day: day, goal_per_day: 0, done_per_day: 0
+      #   )
+      # end
+      # task.rearrange
+      # render json: { controller_action: 'tasks#create', task: task, arrangements: task.arrangements }
     end
     rescue => e
-      render json: {controller_action: 'tasks#create', text: e.message }
+      render json: {controller_action: 'tasks#create', text: e}
   end
 
   def index
