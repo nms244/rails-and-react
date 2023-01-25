559544a9320f6bb3ac179e3e76db925db958f89a
nomos244 <nomos.geek@gmail.com>
2023-01-26 05:38:43 +0900

    いい感じに表示されるよう修正

--- frontend/src/components/pages/DayTasks.jsx
+++ frontend/src/components/pages/DayTasks.jsx
@@ -1,11 +1,11 @@
 import { TaskCard } from '../organisms/TaskCard'
 
 export const DayTasks = (props) => {
-  const { today_tasks } = props
+  const { day_tasks } = props
 
   return (
     <main className="w-full flex flex-wrap m-4 mb-16">
-      {today_tasks.map((task) => (
+      {day_tasks.map((task) => (
         <TaskCard key={task.id} task={task} />
       ))}
     </main>
