c385254a7c3643fd79feff8fa18aa62c16691284
nomos244 <nomos.geek@gmail.com>
2023-01-26 04:04:52 +0900

    arrangementを見られるようにした

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,23 +8,29 @@ import { TaskIndex } from './components/pages/TaskIndex'
 import { DefaultLayout } from './components/templates/DefaultLayout'
 
 function App() {
+  const WDAYS = Object.freeze({sun: 0, mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6})
   axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL
 
   const today = new Date().getDay();
 
   const [day, setDay] = useState(today);
   const [tasks, setTasks] = useState([]);
+  const [arrangements, setArrangements] = useState([]);
 
-  // const day_tasks = tasks.filter((task) => task.days === day)
+  const day_arrangements = arrangements.filter((arr) => WDAYS.arr.day === day)
+  console.log(day);
+  console.log(day_arrangements);
 
   useEffect(() => {
-    const getTasks = async () => {
-      const response = await axios.get(requests.task_index);
-      console.log(response.data.tasks);
-      console.log(today);
-      setTasks(response.data.tasks);
+    const getTasksAndArrangements = async () => {
+      const task_response = await axios.get(requests.task_index);
+      const arrangement_response = await axios.get(requests.arrangement_index);
+      console.log(task_response.data.tasks);
+      console.log(arrangement_response.data.arrangements);
+      setTasks(task_response.data.tasks);
+      setArrangements(arrangement_response.data.arrangements);
     }
-    getTasks();
+    getTasksAndArrangements();
   }, [])
 
   return (
